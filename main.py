4f96b55092db42631619d05b1fbc1f74110b14b5
Jack Anderson <40208539@live.napier.ac.uk>
2017-11-14 18:05:34 +0000

    one hot encoding

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Jack Anderson 2017
 import csv
+import numpy as np
 import pandas as pd
-
+from collections import OrderedDict
 
 NSL_KDD_TRAINING_PATH   = "Datasets/NSL-KDD/KDDTrain.csv"
 NSL_KDD_TRAINING20_PATH = "Datasets/NSL-KDD/KDDTrain_20Percent.csv"
@@ -10,7 +11,16 @@ NSL_KDD_ATTACKS_PATH    = "Datasets/NSL-KDD/KDDAttackTypes.csv"
 NSL_KDD_FIELDS_PATH     = "Datasets/NSL-KDD/KDDFieldNames.csv"
 
 dataset_cols    = list() # [Field Name]
+dataset_types   = list()
 dataset_attacks = dict() # [Attack Name, Attack Type]
+
+nominal_inx = [1, 2, 3]
+binary_inx  = [6, 11, 13, 14, 20, 21]
+numeric_inx = list(set(range(41)).difference(nominal_inx).difference(binary_inx))
+
+nominal_cols = list()
+binary_cols  = list()
+numeric_cols = list()
 
 # Loads values from a two column .csv file and stores in a dictionary object.
 def load_cols(path):
@@ -18,6 +28,13 @@ def load_cols(path):
         reader = csv.reader(file)
         for row in reader:
             dataset_cols.append(row[0])
+            dataset_types.append(row[1])
+    for i in nominal_inx:
+        nominal_cols.append(dataset_cols[i])
+    for i in binary_inx:
+        binary_cols.append(dataset_cols[i])
+    for i in numeric_inx:
+        numeric_cols.append(dataset_cols[i])
 
 def load_attacks(path):
     with open(path, 'r') as file:
@@ -27,7 +44,35 @@ def load_attacks(path):
 
 # Loads a dataset and divides into 8 partitions.
 def load_dataset(path, fields):
-    return pd.read_csv(path, index_col=False, names=dataset_cols)
+    return pd.read_csv(path, index_col=False, names=fields)
+
+def get_attribute_ratio(dataset, numericCols, binaryCols, labelCol):
+    ratio_dict = dict()
+
+    numeric_col_total = dataset[numeric_cols].mean().sum()
+
+    for column in dataset:
+        if column in numericCols:
+            ratio_dict[column] = (dataset[column].mean() / numeric_col_total)
+        elif column in binaryCols:
+            val1 = np.sum(dataset[column] == 1)
+            val2 = np.sum(dataset[column] == 0)
+            val = val1 / val2
+            print(str(val1) + " / " + str(val2) + " = " + str(val))
+            ratio_dict[column] = val
+
+    return OrderedDict(sorted(ratio_dict.items(), key=lambda v: -v[1]))
+
+def one_hot_encoding(dataset, nominalCols):
+    for column in dataset:
+        if column in nominalCols:
+            df_oh = pd.get_dummies(dataset[column])
+            dataset.drop(column, axis=1)
+            dataset = pd.concat([dataset, df_oh], axis=1)
+            for column_oh in df_oh:
+                binary_cols.append(column_oh)
+
+    return dataset
 
 # Main method
 if __name__ == '__main__':
@@ -36,4 +81,12 @@ if __name__ == '__main__':
     load_attacks(NSL_KDD_ATTACKS_PATH)
     # Load dataset
     dataset = load_dataset(NSL_KDD_TRAINING20_PATH, dataset_cols)
-    print(dataset['labels'].to_string())
+    dataset_oh = one_hot_encoding(dataset, nominal_cols)
+
+    #for column in dataset_oh:
+    #    print(str(column))
+
+    ar_dict = get_attribute_ratio(dataset_oh, numeric_cols, binary_cols, 'labels5')
+
+    #for key, value in ar_dict.items():
+    #    print(str(key) + " : " + str(value) + "\n")
