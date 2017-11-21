7135bca1e06dd08bd6c955e476258c7d0fb769a8
Boxfort <40208539@live.napier.ac.uk>
2017-11-21 12:45:18 +0000

    Began unit tests

---
+++ src/test/SwingGUITest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import COMNIA.SwingGUI;
+import org.junit.jupiter.api.Test;
+
+class SwingGUITest
+{
+		@Test
+		public void createSwingGUI()
+		{
+			SwingGUI window = new SwingGUI();
+		}
+}
