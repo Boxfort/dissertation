ba46d13a8fcb49e3448734cdde2a6564d3da4fee
Boxfort <40208539@live.napier.ac.uk>
2018-01-14 15:53:27 +0000

    Changed to lists

--- src/DatasetWindow.py
+++ src/DatasetWindow.py
@@ -9,15 +9,17 @@ class DatasetWindow(QDialog, Ui_Dialog):
         super().__init__()
 
     def show(self):
-        ui = Ui_Dialog()
-        ui.setupUi(self)
+        self.setupUi(self)
         return self.exec_()
 
     def btn_train_set_clicked(self):
         filename = QFileDialog.getOpenFileName(self, 'Select File')
+        self.txt_train_set.setText(filename[0])
 
     def btn_test_set_clicked(self):
         filename = QFileDialog.getOpenFileName(self, 'Select File')
+        self.txt_test_set.setText(filename[0])
 
     def btn_labels_clicked(self):
         filename = QFileDialog.getOpenFileName(self, 'Select File')
+        self.txt_labels.setText(filename[0])
